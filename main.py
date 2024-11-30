e5b6bbae094520c49dae8c5ff3bae6c145c62f11
Seliuzhytski Ivan <ivan.seliushytski17@gmail.com>
2024-11-30 09:50:16 +0300

    first commit

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import json
 with open('dump.json', 'r', encoding='utf-8') as file:
     data = json.load(file)
-numb = input("Введите номер квалификации: ")
-print("\n=============== Результат поиска ===============")
-for item in data:
-    if item['model'] == 'data.skill' and item['fields']['code'].startswith(numb):
-        print(f"{item['fields']['code']} >> {item['fields']['title']}")
-        found = True
-if not found:
-    print("============== Не найдено ===============")
+num = input("Введите номер квалификации: ")
+def findskills():
+    for item in data:
+        if item['model'] == 'data.skill' and item['fields']['code'].startswith(num):
+            print(f"{item['fields']['code']} >> {item['fields']['title']}")
+if findskills:
+    print("\n==================== Найдено ====================")
+    findskills()
+else:
+    print("==================== Не найдено ====================")
